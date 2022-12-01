8ffd5c82639d59590568911cf8e69ec3ceb33993
Marta_Meneses <menesesmmarta@gmail.com>
2022-12-01 12:08:18 +0100

    errors all-tab

---
+++ Models/Course.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Course{
+    //properties
+    [Key]
+    public int CourseID { get; set; }
+    public string CourseName { get; set; }
+    
+    //Relations
+    public List<Student> students;
+    public List<Teacher> teachers;
+    public List<CourseClass> courseclasses;
+   
+}
