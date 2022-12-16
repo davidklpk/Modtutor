57fc76c0b15acc17d4a53ae0a01960c539d1f882
Marta_Meneses <menesesmmarta@gmail.com>
2022-12-16 12:28:33 +0100

    grade and icon updated

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<DashContext>(options => 
 //options.UseSqlServer(builder.Configuration.GetConnectionString("SQLSERVERCONNECTIONRiciano")));
-options.UseSqlServer(builder.Configuration.GetConnectionString("SQLSERVERCONNECTIONDavid")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("SQLSERVERCONNECTIONMarta")));
 builder.Services.AddCors(options => options.AddPolicy(name: "DashOrigins",
 policy => {
     policy.WithOrigins("http://localhost:4200/").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
